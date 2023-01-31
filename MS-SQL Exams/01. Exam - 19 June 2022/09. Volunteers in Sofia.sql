1cbaef6907527e90384bb9aa9a0650e0d0bc2678
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-01-31 20:42:08 +0200

    Update 09. Volunteers in Sofia.sql

--- MS-SQL Exams/01. Exam - 19 June 2022/09. Volunteers in Sofia.sql
+++ MS-SQL Exams/01. Exam - 19 June 2022/09. Volunteers in Sofia.sql
@@ -1,14 +1,9 @@
 SELECT
     v.Name,
     v.PhoneNumber,
-    SUBSTRING(v.Address, CHARINDEX(',', v.Address) + 2, LEN(v.Address) - CHARINDEX(',', v.Address))
+    SUBSTRING(v.Address, CHARINDEX(',', v.Address) + 2, LEN(v.Address) - CHARINDEX(',', v.Address))    
 FROM Volunteers AS v
-WHERE 
-    v.Address LIKE '%Sofia%' AND 
-    v.DepartmentId = 
-        (
-          SELECT Id
-          FROM VolunteersDepartments 
-          WHERE DepartmentName = 'Education program assistant'
-        )
+JOIN VolunteersDepartments AS vd ON v.DepartmentId = vd.Id
+WHERE vd.DepartmentName = 'Education program assistant' AND
+    v.Address LIKE '%Sofia%'
 ORDER BY v.Name ASC
