b6610dd7a9c7026525978fe8a8726fdb623d3925
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-02 20:54:20 +0200

    Update 12. Full Info for Airports.sql

--- MS-SQL Exams/02. Retake Exam - 10 Dec 2021/12. Full Info for Airports.sql
+++ MS-SQL Exams/02. Retake Exam - 10 Dec 2021/12. Full Info for Airports.sql
@@ -1,22 +1,23 @@
 CREATE PROC usp_SearchByAirportName
-(@airportName VARCHAR(70))
+(@airportName varchar(70))
 AS
+BEGIN
 SELECT
-    ap.AirportName,
+    a.AirportName,
     p.FullName,
-        CASE WHEN fd.TicketPrice <= 400 THEN 'Low'
-        ELSE 
-            CASE WHEN fd.TicketPrice <= 1500 THEN 'Medium'
-            ELSE 'High'
-            END 
-        END AS LevelOfTicketPrice,
+    CASE 
+        WHEN (fd.TicketPrice <= 400) THEN 'Low'
+        WHEN (fd.TicketPrice <= 1500) THEN 'Medium'
+        ELSE 'High'
+    END AS LevelOfTickerPrice,
     ac.Manufacturer,
     ac.Condition,
-    act.TypeName
-FROM Airports AS ap
-JOIN FlightDestinations AS fd ON ap.Id = fd.AirportId
+    at.TypeName
+FROM Airports AS a
+JOIN FlightDestinations AS fd ON a.Id = fd.AirportId
 JOIN Passengers AS p ON fd.PassengerId = p.Id
 JOIN Aircraft AS ac ON fd.AircraftId = ac.Id
-JOIN AircraftTypes AS act ON ac.TypeId = act.Id
-WHERE ap.AirportName = @airportName
-ORDER BY Manufacturer, FullName
+JOIN AircraftTypes AS at ON ac.TypeId = at.Id
+WHERE AirportName = @airportName
+ORDER BY Manufacturer, p.FullName
+END
