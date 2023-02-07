ae313ccede8b85bb66e478258247799808dfa41b
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-07 20:33:23 +0200

    Update 10. Missing Parts.sql

--- MS-SQL Exams/05. Exam - 27 Jun 2020/10. Missing Parts.sql
+++ MS-SQL Exams/05. Exam - 27 Jun 2020/10. Missing Parts.sql
@@ -1,20 +1,15 @@
 SELECT
     p.PartId,
-    p.Description,
+    p.[Description],
     SUM(pn.Quantity) AS Required,
     SUM(p.StockQty) AS InStock,
-    ISNULL(SUM(t.Quantity), 0) AS Ordered
+    ISNULL(SUM(op.Quantity), 0) AS Ordered
 FROM Parts AS p
 LEFT JOIN PartsNeeded AS pn ON p.PartId = pn.PartId
 LEFT JOIN Jobs AS j ON pn.JobId = j.JobId
-LEFT JOIN
-    (
-        SELECT PartId, Quantity        
-        FROM Orders AS o
-        JOIN OrderParts AS op ON o.OrderId = op.OrderId
-        WHERE o.Delivered = 0    
-    ) AS t ON p.PartId = t.PartId
-WHERE j.Status <> 'Finished'
-GROUP BY p.PartId, p.Description
-HAVING SUM(pn.Quantity) > SUM(p.StockQty) + ISNULL(SUM(t.Quantity), 0)
-ORDER BY p.PartId ASC
+LEFT JOIN Orders AS o ON j.JobId = o.JobId
+LEFT JOIN OrderParts AS op ON o.OrderId = op.OrderId
+WHERE j.[Status] <> 'Finished'
+GROUP BY p.PartId, p.[Description]
+HAVING SUM(pn.Quantity) > SUM(p.StockQty) + ISNULL(SUM(op.Quantity), 0)
+ORDER BY p.PartId
