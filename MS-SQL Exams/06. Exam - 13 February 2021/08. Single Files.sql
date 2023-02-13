b08b845750c05d1fc7f870f77525139faf053423
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-13 19:18:18 +0200

    Update 08. Single Files.sql

--- MS-SQL Exams/06. Exam - 13 February 2021/08. Single Files.sql
+++ MS-SQL Exams/06. Exam - 13 February 2021/08. Single Files.sql
@@ -1,11 +1,8 @@
-SELECT 
-    Id, 
-    Name, 
-    CAST(Size AS VARCHAR) + 'KB' AS Size
-FROM Files
-WHERE Id NOT IN 
-(
-    SELECT ParentId 
-    FROM Files 
-    WHERE ParentId IS NOT NULL
-)
+SELECT
+    f1.Id,
+    f1.Name,
+    CAST(f1.Size AS varchar) + 'KB'
+FROM Files AS f1
+LEFT JOIN Files AS f2 ON f1.Id = f2.ParentId
+WHERE f2.Id IS NULL
+ORDER BY f1.Id ASC, f1.Name ASC, f1.Size DESC
