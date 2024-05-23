45e8631ec1fcada18b7e18f13dfc8de3ded19833
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2024-05-23 19:56:03 +0300

    Add files via upload

---
+++ WebFundamentals Exams/00. 19 Jun 2023 - Homies/Homies/Contracts/IEventService.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Homies.Contracts
+{
+    using Models;
+
+    public interface IEventService
+    {
+        Task<IEnumerable<AllEventViewModel>> GetAllEventsAsync();
+
+        Task<IEnumerable<AllEventViewModel>> GetUserJoinedEventsAsync(string userId);
+
+        Task<List<TypeDataModel>> GetAllTypesAsync();
+
+        Task AddNewEventAsync(string userId, DateTime validStartDate, DateTime validEndDate, EventFormModel model);
+
+        Task JoinEventAsync(string userId, int id);
+
+        Task LeaveEventAsync(string userId, int id);
+
+        Task<EventFormModel> GetEventForEditAsync(string userId, int id);
+
+        Task EditEventAsync(string userId, int id, DateTime startDate, DateTime endDate, EventFormModel model);
+
+        Task<DetailsViewModel> GetDetailsAsync(int id);
+    }
+}
