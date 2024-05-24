9467d5c9ea7fcd7e64cab5791270c206c152ff8d
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2024-05-24 18:20:30 +0300

    Add files via upload

---
+++ WebFundamentals Exams/00. 19 Jun 2023 - Homies/Homies/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Homies.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("All", "Event");
+            }
+
+            return View();
+        }
+    }
+}
