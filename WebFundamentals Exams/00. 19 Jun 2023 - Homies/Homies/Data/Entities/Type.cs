35a7805f9c66a05c37933380bf88362649dbe0d7
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2024-05-25 17:37:44 +0300

    Add files via upload

---
+++ WebFundamentals Exams/00. 19 Jun 2023 - Homies/Homies/Data/Entities/Type.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Homies.Data.Entities
+{
+    using static Data.DataConstants.Type;
+
+    public class Type
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(TypeNameMaxLength)]
+        public string Name { get; set; } = null!;
+        
+        public HashSet<Event> Events { get; set; } = new();
+    }
+}
