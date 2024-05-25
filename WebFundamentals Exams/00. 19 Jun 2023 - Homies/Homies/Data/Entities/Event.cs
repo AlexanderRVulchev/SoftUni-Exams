4e8ec2d2a0a77d3dc0952e8ffa7e11b29594f415
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2024-05-25 17:37:22 +0300

    Add files via upload

---
+++ WebFundamentals Exams/00. 19 Jun 2023 - Homies/Homies/Data/Entities/Event.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Homies.Data.Entities
+{
+    using static Data.DataConstants.Event;
+
+    public class Event
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; } = null!;
+        
+        [Required]
+        [MaxLength(DescriptionMaxLength)]
+        public string Description { get; set; } = null!;
+        
+        [ForeignKey(nameof(Organiser))]
+        public string OrganiserId { get; set; } = null!;
+        public IdentityUser Organiser { get; set; } = null!;
+        
+        public DateTime CreatedOn { get; set; }
+        
+        public DateTime Start { get; set; }
+        
+        public DateTime End { get; set; }
+        
+        [ForeignKey(nameof(Type))]
+        public int TypeId { get; set; }
+        public Type Type { get; set; } = null!;
+        
+        public HashSet<EventParticipant> EventsParticipants = new();
+    }
+}
