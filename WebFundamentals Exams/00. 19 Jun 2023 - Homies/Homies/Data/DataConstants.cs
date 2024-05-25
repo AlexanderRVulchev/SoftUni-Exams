6fc92acb603aeb1b3eba090005458d60b1696002
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2024-05-25 17:36:42 +0300

    Add files via upload

---
+++ WebFundamentals Exams/00. 19 Jun 2023 - Homies/Homies/Data/DataConstants.cs
@@ -0,0 +1,20 @@
+﻿namespace Homies.Data
+{
+    public class DataConstants
+    {
+        public class Event
+        {
+            public const int NameMinLength = 5;
+            public const int NameMaxLength = 20;
+
+            public const int DescriptionMinLength = 15;
+            public const int DescriptionMaxLength = 150;
+        }
+
+        public class Type
+        {
+            public const int TypeNameMinLength = 5;
+            public const int TypeNameMaxLength = 15;
+        }
+    }
+}
