f3d390cc792f31d723aa51dacf149cb164a989f2
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2024-05-27 21:01:59 +0300

    Add files via upload

---
+++ WebFundamentals Exams/00. 19 Jun 2023 - Homies/Homies/Models/EventFormModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Homies.Models
+{
+    using static Data.DataConstants.Event;
+
+    public class EventFormModel
+    {
+        [Required]
+        [StringLength(NameMaxLength, MinimumLength = NameMinLength)]
+        public string Name { get; set; } = null!;
+ 
+        [Required]
+        [StringLength(DescriptionMaxLength, MinimumLength = DescriptionMinLength)]
+        public string Description { get; set; } = null!;
+ 
+        [Required(AllowEmptyStrings = false)]
+        public string Start { get; set; } = null!;
+
+        [Required(AllowEmptyStrings = false)]
+        public string End { get; set; } = null!;
+ 
+        public List<TypeDataModel> Types { get; set; } = new();
+ 
+        public int TypeId { get; set; }
+    }
+}
