abe3383aba5d79d5c4bc592825634e313aeb9e28
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2024-05-27 21:01:27 +0300

    Add files via upload

---
+++ WebFundamentals Exams/00. 19 Jun 2023 - Homies/Homies/Models/AllEventViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Homies.Models
+{
+    public class AllEventViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Start { get; set; } = null!;
+
+        public string Type { get; set; } = null!;
+
+        public string Organiser { get; set; } = null!;
+    }
+}
