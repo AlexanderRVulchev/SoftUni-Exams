48a7eb944a76b53bbc442bd1f56171546595bc91
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2024-05-27 21:01:42 +0300

    Add files via upload

---
+++ WebFundamentals Exams/00. 19 Jun 2023 - Homies/Homies/Models/DetailsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Homies.Models
+{
+    public class DetailsViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        [Display(Name = "Starting time")]
+        public string Start { get; set; } = null!;
+
+        [Display(Name = "Estimated ending time")]
+        public string End { get; set; } = null!;
+
+        public string Organiser { get; set; } = null!;
+
+        [Display(Name = "Created on")]
+        public string CreatedOn { get; set; } = null!;
+
+        public string Type { get; set; } = null!;
+    }
+}
