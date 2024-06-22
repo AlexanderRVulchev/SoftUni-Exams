c5e3b6c9a89ada3fa9bff10b4695c9800d4b7527
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2024-06-22 20:43:08 +0300

    Add files via upload

---
+++ WebFundamentals Exams/01. 21 Dec 2022 - Contacts/Contacts/Data/ContactsDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Contacts.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contacts.Data
+{
+    public class ContactsDbContext : IdentityDbContext<ApplicationUser>
+    {
+
+        public ContactsDbContext(DbContextOptions<ContactsDbContext> options)
+            : base(options)
+        {
+            this.Database.Migrate();
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<ApplicationUserContact> ApplicationUsersContacts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUserContact>(e =>
+            {
+                e.HasKey(uc => new { uc.ApplicationUserId, uc.ContactId });
+            });
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
