6fce83f05681c0c6f247bf5c37f50b451c13ee85
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2024-06-23 15:42:55 +0300

    Add files via upload

---
+++ WebFundamentals Exams/01. 21 Dec 2022 - Contacts/Contacts/Data/Entities/Contact.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Contacts.Data.Entities
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(30)]
+        public string Email { get; set; }
+
+        [Required]
+        [Phone]
+        [StringLength(14)]
+        public string PhoneNumber { get; set; }
+        public string? Address { get; set; }
+        public string? Website { get; set; }
+        public ICollection<ApplicationUserContact> ApplicationUsersContacts { get; set; }
+
+    }
+}
