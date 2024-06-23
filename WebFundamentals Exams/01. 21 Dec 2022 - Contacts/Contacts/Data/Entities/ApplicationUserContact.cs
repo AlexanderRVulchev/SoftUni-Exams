9b13bd57a3421c25db8da67f3de592c84d2e1711
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2024-06-23 15:42:46 +0300

    Add files via upload

---
+++ WebFundamentals Exams/01. 21 Dec 2022 - Contacts/Contacts/Data/Entities/ApplicationUserContact.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Contacts.Data.Entities
+{
+    public class ApplicationUserContact
+    {
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationUserId { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+
+        [ForeignKey("Contact")]
+        public int ContactId { get; set; }
+        public Contact Contact { get; set; }
+    }
+}
