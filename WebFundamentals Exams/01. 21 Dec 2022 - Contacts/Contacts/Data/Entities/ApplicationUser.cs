556bdd7159c0140d4e63ce830dfef086aab44e63
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2024-06-23 15:42:31 +0300

    Add files via upload

---
+++ WebFundamentals Exams/01. 21 Dec 2022 - Contacts/Contacts/Data/Entities/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Contacts.Data.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ICollection<ApplicationUserContact> ApplicationUsersContacts { get; set; }
+    }
+}
