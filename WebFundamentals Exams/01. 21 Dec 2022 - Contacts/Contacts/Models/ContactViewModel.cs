7c9dac69b76ab8828b3d535377e565e372669163
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2024-06-25 20:59:54 +0300

    Add files via upload

---
+++ WebFundamentals Exams/01. 21 Dec 2022 - Contacts/Contacts/Models/ContactViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Contacts.Models
+{
+    public class ContactViewModel
+    {
+        public int Id { get; init; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? Website { get; set; }
+    }
+}
