146d848fd598ff38443123117e1278ec52f319c3
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2024-06-25 20:59:37 +0300

    Add files via upload

---
+++ WebFundamentals Exams/01. 21 Dec 2022 - Contacts/Contacts/Models/ContactFormModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+using static Contacts.Data.DataConstants.Contact;
+
+namespace Contacts.Models
+{
+    public class ContactFormModel
+    {
+        [Required]
+        [StringLength(ContactFirstNameMaxLength, MinimumLength = ContactFirstNameMinLength)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(ContactLastNameMaxLength, MinimumLength = ContactLastNameMinLength)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "E-mail")]
+        [StringLength(ContactEmailMaxLength, MinimumLength = ContactEmailMinLength)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(ContactPhoneNumberMaxLength, MinimumLength = ContactPhoneNumberMinLength)]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+
+        public string? Address { get; set; }
+
+        [Required]
+        [RegularExpression(ContactWebsiteRegex)]
+        public string Website { get; set; }
+    }
+
+}
