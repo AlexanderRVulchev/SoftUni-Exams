c3fb56ceb4786e08f1b7decd70cb7f488b6e64af
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2024-06-25 20:59:19 +0300

    Add files via upload

---
+++ WebFundamentals Exams/01. 21 Dec 2022 - Contacts/Contacts/Models/AllContactsQueryModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Contacts.Models
+{
+    public class AllContactsQueryModel
+    {
+        public IEnumerable<ContactViewModel> Contacts { get; set; }
+            = new List<ContactViewModel>();
+    }
+}
